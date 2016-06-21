6effb20b62b03b1b0786d76eea985b5e6963228e
oscar <oscaroceguera@gmail.com>
2016-06-21 01:25:32 -0600

    How to improve templates in backbone js

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ var Person = Backbone.Model.extend({
 
 var PersonView = Backbone.View.extend({
 	tagName: 'li',
-	my_template: _.template("<strong><%= name %></strong> (<%= age %>) - <%= occupation %>"),
+	template: _.template($('#personTemplate').html()),
 	initialize: function () {
 		this.render()
 	},
 	render: function() {
 		this.$el.html(
-			this.my_template(this.model.toJSON())
+			this.template(this.model.toJSON())
 		)
 	}
 })
