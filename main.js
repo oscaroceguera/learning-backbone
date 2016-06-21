aac1b4534b8750edb949f8a9f6e2160d6eeac4b1
oscar <oscaroceguera@gmail.com>
2016-06-21 01:05:15 -0600

    Adding Validations in models backbone js

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ var Person = Backbone.Model.extend({
 		age: 25,
 		occupation: 'Worker'
 	},
+	validate: function (attributes) {
+		if (attributes.age < 0) {
+			return 'Age must be positive'
+		}
+
+		if (!attributes.name) {
+			return 'Every person must have a name'
+		}
+	},
 	work: function () {
 		return this.get('name') + 'is working'
 	}
